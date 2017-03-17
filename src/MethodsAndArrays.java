94e63c99ee5129a97868037f239c43a74db038d2
Alan Tse <starttodevelop@gmail.com>
2017-03-17 16:13:04 +1100

    Added driver class

---
+++ src/MethodsAndArrays.java
@@ -0,0 +1,15 @@
+
+public class MethodsAndArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long myLongVariable = 100000L;
+		int myIntVariable;
+		myIntVariable = (int) myLongVariable;
+		System.out.println("myIntVariable: " + myIntVariable);
+		System.out.println("myLongVariable: " + myLongVariable);
+		
+
+	}
+
+}
